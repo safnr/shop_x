5e58d0c7c55ed53082d7439392b5fe8134a5ce52
safnr <55081040+safnr@users.noreply.github.com>
2025-11-30 14:20:56 +0700

    add category model

--- lib/model/category_model.dart
+++ lib/model/category_model.dart
@@ -4,4 +4,13 @@ class CategoryModel {
   final String slug;
 
   CategoryModel({required this.name, required this.url, required this.slug});
+
+  factory CategoryModel.fromMap(Map<String, dynamic> map) {
+    return CategoryModel(
+      name: map['name'],
+      url: map['url'],
+      slug: map['slug'],
+    );
+  }
+
 }
