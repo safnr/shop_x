3475f9118f618a41e4c2eb2d6d88bd52efe9f652
safnr <55081040+safnr@users.noreply.github.com>
2025-12-01 10:02:45 +0700

    add function to fetch product detail data

--- lib/features/main/home/product_provider.dart
+++ lib/features/main/home/product_provider.dart
@@ -6,11 +6,24 @@ import 'package:shop_x/model/category_model.dart';
 import 'package:shop_x/model/product_model.dart';
 import 'package:shop_x/utils/constants.dart';
 
+final fetchProductById = FutureProvider.family<ProductModel?, int>((
+  ref,
+  productId,
+) async {
+  final response = await http.get(Uri.parse("$baseUrl/products/$productId"));
+  if (response.statusCode == 200) {
+    final decode = jsonDecode(response.body);
+    final product = ProductModel.fromMap(decode);
+    return product;
+  }
+  return null;
+});
+
 final fetchProductsProvider = FutureProvider((ref) async {
   final response = await http.get(Uri.parse("$baseUrl/products"));
 
   final decode = jsonDecode(response.body);
-  
+
   if (response.statusCode == 200) {
     final rawProducts = decode["products"];
     List<ProductModel> products = [];
@@ -22,7 +35,7 @@ final fetchProductsProvider = FutureProvider((ref) async {
   throw Exception('Failed to fetch product');
 }); // FutureProvider
 
-final fetchCategoryProductProovider = FutureProvider((ref) async {
+final fetchCategoryProductProvider = FutureProvider((ref) async {
   final response = await http.get(Uri.parse("$baseUrl/products/categories"));
 
   // Decode, convert dari String ke Object
--- lib/features/products/ui/detail_product_screen.dart
+++ lib/features/products/ui/detail_product_screen.dart
@@ -1,1 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shop_x/features/main/home/product_provider.dart';
 
+class DetailProductScreen extends ConsumerWidget {
+  const DetailProductScreen({super.key, required this.productId});
+
+  final int productId;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final productState = ref.watch(fetchProductById(productId));
+
+    return Scaffold(
+      body: productState.when(
+        data: (data) => Center(child: Text("Product name: ${data?.title}")),
+        error: (error, stackTrace) =>
+            Center(child: Text(error.toString())), // Center
+        loading: () =>
+            const Center(child: CircularProgressIndicator.adaptive()),
+      ),
+    );
+  }
+}
